3b53602ce47aa8c0debc32f9c28c6d1aef61413c
Vitor Borgatte <79037838+vtbrgt@users.noreply.github.com>
2023-02-14 14:33:45 -0300

    express
    criando servidor agora com a biblioteca express

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Person } = require('./person');
 
 // require('./modules/path');
 // require('./modules/fs');
-require('./modules/http')
+// require('./modules/http')
+require('./modules/express')
 
 const person = new Person('Vitor');
